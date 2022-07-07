9c7ff987fd100e5709296bc1e82f9afacc990aaf
DarrenWong <76094766+Applewhz@users.noreply.github.com>
2022-07-07 22:28:18 +0800

    Update Routes.jsx
    added authentication for route
    
    added proptypes for props

--- react-fundamentals-course/src/Routes/Routes.jsx
+++ react-fundamentals-course/src/Routes/Routes.jsx
@@ -7,22 +7,56 @@ import { Courses } from '../components/Courses/Courses'
 import Home from '../components/Home/Home'
 import { CreateCourse } from '../components/CreateCourse/CreateCourse'
 import { Header } from '../components/Header/Header'
+import PropTypes from 'prop-types'
+import { Navigate, useLocation} from "react-router-dom";
 
-const AppRoutes = (props) => {
+export const ProtectedRoute = ({children}) => {
+  let location = useLocation();
+  if(!localStorage.getItem("token")){
+    return <Navigate  to="/login" state={{ from: location }}  replace />;
+  }
+  return children;
+};
+
+
+
+const AppRoutes = ({courseList, authorList, addCourseHandler, addAuthorHandler}) => {
   return (
     <Router forceRefresh={true}>
         <Header />
       <Routes>
         <Route exact path='/' element={<Login />}/>
         <Route path='/login' element={<Login />}/>
-        <Route exact path='/courses' element={<Courses courseList={props.courseList} authorList={props.authorList}/>}/>
-        <Route path='/courses/add' element={<CreateCourse courseList={props.courseList} authorList={props.authorList} addCourseHandler={props.addCourseHandler} addAuthorHandler={props.addAuthorHandler}/>}/>
-        <Route path='/courses/:id' element={<CourseInfo courseList={props.courseList} authorList={props.authorList} />}/>
         <Route path='/register' element={<Registration />}/>
-        <Route path='/courseInfo' element={<CourseInfo />}/>
+        <Route exact path='/courses' element={
+        <ProtectedRoute>
+          <Courses courseList={courseList} authorList={authorList}/>
+        </ProtectedRoute>
+        }/>
+        <Route path='/courses/add' element={
+          <ProtectedRoute>
+            <CreateCourse courseList={courseList} authorList={authorList} addCourseHandler={addCourseHandler} addAuthorHandler={addAuthorHandler}/>
+          </ProtectedRoute>
+        }/>
+        <Route path='/courses/:id' element={
+          <ProtectedRoute>
+            <CourseInfo courseList={courseList} authorList={authorList} />
+          </ProtectedRoute>
+        }/>
+        <Route path='/courseInfo' element={
+          <ProtectedRoute>
+            <CourseInfo />
+          </ProtectedRoute>
+        }/>
       </Routes>
     </Router>
   )
 }
 
+AppRoutes.propTypes = {
+  courseList: PropTypes.object,
+  authorList: PropTypes.object,
+  addCourseHandler: PropTypes.func,
+  addAuthorHandler: PropTypes.func
+}
 export default AppRoutes
