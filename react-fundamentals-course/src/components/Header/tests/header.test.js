28a7b4ec6c22bccdc0362e1ef925c7f067b948ef
DarrenWong <76094766+Applewhz@users.noreply.github.com>
2022-08-10 10:42:40 +0800

    Update header.test.js

--- react-fundamentals-course/src/components/Header/tests/header.test.js
+++ react-fundamentals-course/src/components/Header/tests/header.test.js
@@ -4,12 +4,12 @@ import { Header } from '../Header.jsx'
 import { Provider } from 'react-redux'
 import store from '../../../store/index'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
-import configureMockStore from 'redux-mock-store'
-import createSagaMiddleware from 'redux-saga'
-import {mockSaga} from 'redux-saga-mock'
+// import configureMockStore from 'redux-mock-store'
+// import createSagaMiddleware from 'redux-saga'
+// import {mockSaga} from 'redux-saga-mock'
 
-const sagaMiddleware = createSagaMiddleware()
-const mockInitialStore = configureMockStore(sagaMiddleware)
+// const sagaMiddleware = createSagaMiddleware()
+// const mockInitialStore = configureMockStore(sagaMiddleware)
 afterEach(cleanup)
 const getHeader = (mockStore) => {
   return (
@@ -23,17 +23,17 @@ const getHeader = (mockStore) => {
 
 describe('Header', () => {
   it('renders Header component', async () => {
-    const mockUserStore = mockInitialStore({
+    const mockUserStore = ({
       ...store,
-    //   user: {
-    //       userDetails: [{
-    //     isAuth: true,
-    //     name: 'tester',
-    //     email: 'tester@email.com',
-    //     token: 'Mock token here',
-    //     role: 'user',
-    //   }]
-    // },
+      user: {
+          userDetails: [{
+        isAuth: true,
+        name: 'tester',
+        email: 'tester@email.com',
+        token: 'Mock token here',
+        role: 'user',
+      }]
+    },
     })
     const {getByTestId, debug} = render(getHeader(mockUserStore))
     // const testImage = document.querySelector("img") as HTMLImageElement;
